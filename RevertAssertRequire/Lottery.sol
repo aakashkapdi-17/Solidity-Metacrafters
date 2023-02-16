657394c67fb6b3d4b04268b345f0fdedd90f818a
aakashkapdi-17 <aakash.learning17@gmail.com>
2023-02-16 19:27:13 +0530

    lottery

--- RevertAssertRequire/Lottery.sol
+++ RevertAssertRequire/Lottery.sol
@@ -1,11 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.6.0 <0.9.0;
 
-// it should be considered a normal and healthy occurrence for a require() statement 
-// to fail (same with revert()). When an assert() statement fails, something very wrong and 
-// unexpected has happened,and you need to fix your code.
-
-
 contract Lottery{
     address payable owner;
     address[] players;
